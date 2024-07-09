403c7dc30c76d72c9e5522e33877ff08ec4f9d1d
XiaoYoung <xy_23@outlook.com>
2024-07-09 18:25:05 +0800

    add ws2812

---
+++ io/ws2812/ws2812.cpp
@@ -0,0 +1,21 @@
+#include "ws2812.hpp"
+
+namespace io
+{
+constexpr uint8_t CODE0 = 0xC0;
+constexpr uint8_t CODE1 = 0xF0;
+
+WS2812::WS2812(SPI_HandleTypeDef * hspi) : hspi_(hspi) {}
+
+void WS2812::set(uint8_t r, uint8_t g, uint8_t b)
+{
+  for (int i = 0; i < 8; i++) {
+    buff_[07 - i] = (((g >> i) & 0x01) ? CODE1 : CODE0) >> 1;
+    buff_[15 - i] = (((r >> i) & 0x01) ? CODE1 : CODE0) >> 1;
+    buff_[23 - i] = (((b >> i) & 0x01) ? CODE1 : CODE0) >> 1;
+  }
+
+  HAL_SPI_Transmit(hspi_, buff_, 24, 0xFFFF);  // dismiss return
+}
+
+}  // namespace io
---
+++ io/ws2812/ws2812.hpp
@@ -0,0 +1,21 @@
+#ifndef IO__WS2812_HPP
+#define IO__WS2812_HPP
+
+#include "spi.h"
+
+namespace io
+{
+class WS2812
+{
+public:
+  WS2812(SPI_HandleTypeDef * hspi);
+  void set(uint8_t r, uint8_t g, uint8_t b);
+
+private:
+  SPI_HandleTypeDef * hspi_;
+  uint8_t buff_[24];
+};
+
+}  // namespace io
+
+#endif  // IO__WS2812_HPP
