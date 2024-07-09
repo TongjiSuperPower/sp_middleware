6c1ad862f3d2de617b13ea3ca3a0a6c788d66d45
XiaoYoung <xy_23@outlook.com>
2024-07-09 15:28:06 +0800

    change format

--- io/buzzer/buzzer.hpp
+++ io/buzzer/buzzer.hpp
@@ -7,19 +7,19 @@ namespace io
 {
 class Buzzer
 {
-private:
-  TIM_HandleTypeDef * htim_;
-  uint16_t channel_;
-  float clock_hz_;
-  uint16_t arr_;
-  uint16_t ccr_;
-
 public:
   Buzzer(TIM_HandleTypeDef * htim, uint16_t channel, float clock_hz);
 
   void start();
   void stop();
   void set(float hz, float duty_cycle = 0.5);
+
+private:
+  TIM_HandleTypeDef * htim_;
+  uint16_t channel_;
+  float clock_hz_;
+  uint16_t arr_;
+  uint16_t ccr_;
 };
 
 }  // namespace io
