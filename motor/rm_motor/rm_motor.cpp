55bd16ea4a91f134cafe20f21a7769ece35c58f6
XiaoYoung <xy_23@outlook.com>
2024-07-21 16:05:53 +0800

    add m2006

--- motor/rm_motor/rm_motor.cpp
+++ motor/rm_motor/rm_motor.cpp
@@ -12,7 +12,6 @@ void RM_Motor::read(uint8_t * data)
   angle_ecd_ = static_cast<uint16_t>((data[0] << 8) | data[1]);
   speed_rpm_ = static_cast<int16_t>((data[2] << 8) | data[3]);
   current_raw_ = static_cast<int16_t>((data[4] << 8) | data[5]);
-  temperature_ = data[6];
 }
 
 void RM_Motor::write(uint8_t * data) const
@@ -20,6 +19,16 @@ void RM_Motor::write(uint8_t * data) const
   data[(motor_id_ - 1) % 4 * 2 + 0] = static_cast<uint8_t>(cmd_raw_ >> 8);
   data[(motor_id_ - 1) % 4 * 2 + 1] = static_cast<uint8_t>(cmd_raw_);
 }
+
+float RM_Motor::angle() const
+{
+  // TODO multicicrle
+  return static_cast<float>(angle_ecd_ - 4095) / 8192 * 2 * tools::PI;
+}
+
+float RM_Motor::speed() const { return static_cast<float>(speed_rpm_) / 60 * 2 * tools::PI; }
+
+int16_t RM_Motor::current_raw() const { return current_raw_; }
 
 void RM_Motor::cmd_raw(int16_t raw) { cmd_raw_ = raw; }
 
--- motor/rm_motor/rm_motor.hpp
+++ motor/rm_motor/rm_motor.hpp
@@ -13,16 +13,20 @@ public:
   void read(uint8_t * data);
   void write(uint8_t * data) const;
 
+  float angle() const;
+  float speed() const;
+  int16_t current_raw() const;
+
   void cmd_raw(int16_t raw);
 
 protected:
   uint8_t motor_id_;
+
+private:
   uint16_t angle_ecd_;
   int16_t speed_rpm_;
   int16_t current_raw_;
-  uint8_t temperature_;
 
-private:
   int16_t cmd_raw_;
 };
 
