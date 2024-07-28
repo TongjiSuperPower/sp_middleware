35c320d56d4eabba40148d5e0c22338973498d68
chmwt <1102453984@qq.com>
2024-07-28 17:46:22 +0800

    改得更合理一点

--- io/led/led.cpp
+++ io/led/led.cpp
@@ -5,9 +5,9 @@ namespace io
 
 Led::Led(TIM_HandleTypeDef * htim) : htim_(htim)
 {
-  HAL_TIM_PWM_Start(&htim5, TIM_CHANNEL_1);
-  HAL_TIM_PWM_Start(&htim5, TIM_CHANNEL_2);
-  HAL_TIM_PWM_Start(&htim5, TIM_CHANNEL_3);
+  HAL_TIM_PWM_Start(htim_, TIM_CHANNEL_1);
+  HAL_TIM_PWM_Start(htim_, TIM_CHANNEL_2);
+  HAL_TIM_PWM_Start(htim_, TIM_CHANNEL_3);
 }
 
 void Led::set(uint16_t red, uint16_t green, uint16_t blue)
