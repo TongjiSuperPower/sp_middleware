e7a8d7955f9079d51bc99334638f5f9b84f85ca9
chmwt <1102453984@qq.com>
2024-09-07 21:18:53 +0800

    使积分项乘上Dt

--- tools/pid/pid.cpp
+++ tools/pid/pid.cpp
@@ -15,12 +15,10 @@ PID::PID(float dt, float kp, float ki, float kd, float max_out, float max_iout, 
 
 void PID::calc(float set, float fdb)
 {
-  if (set != this->data.set) this->data.iout /= 2.0f;
-
   // 微分先行
   this->data.dbuf[2] = this->data.dbuf[1];
   this->data.dbuf[1] = this->data.dbuf[0];
-  this->data.dbuf[0] = (this->data.fdb - fdb) / dt_;
+  this->data.dbuf[0] = (this->data.fdb - fdb);
 
   // 滤波
   this->data.dbuf[0] = alpha_ * this->data.dbuf[0] + (1.0f - alpha_) * this->data.dbuf[1];
@@ -35,10 +33,10 @@ void PID::calc(float set, float fdb)
   // Kp
   this->data.pout = kp_ * this->data.err[0];
   // Ki,梯形积分
-  this->data.iout += ki_ * (this->data.err[0] + this->data.err[1]) / 2.0f;
+  this->data.iout += ki_ * (this->data.err[0] + this->data.err[1]) / 2.0f * dt_;
   this->data.iout = limit_max(this->data.iout, max_iout_);
   // Kd
-  this->data.dout = kd_ * this->data.dbuf[0];
+  this->data.dout = kd_ * this->data.dbuf[0] / dt_;
 
   this->out = limit_max(this->data.pout + this->data.iout + this->data.dout, max_out_);
 }
