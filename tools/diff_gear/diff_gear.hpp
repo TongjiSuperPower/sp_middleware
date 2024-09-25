9c8615f8f184f6cef2f7bdddbfa00d679ae50650
XiaoYoung <xy_23@outlook.com>
2024-09-25 19:22:08 +0800

    update diff_gear

--- tools/diff_gear/diff_gear.hpp
+++ tools/diff_gear/diff_gear.hpp
@@ -6,11 +6,16 @@ namespace tools
 class DiffGear
 {
 public:
+  // reverse_l: left反向旋转
+  // reverse_r: right反向旋转
   DiffGear(bool reverse_l = false, bool reverse_r = true);
 
-  float v_left;
-  float v_right;
+  float v_left;   // 只读! calc()计算结果, left转速, 单位: rad/s
+  float v_right;  // 只读! calc()计算结果, right转速, 单位: rad/s
 
+  // 末端转速 -> 各齿轮转速
+  // v_pitch: 大拇指朝左，右手螺旋方向转速, 单位: rad/s
+  // v_roll: 大拇指朝前，右手螺旋方向转速, 单位: rad/s
   void calc(float v_pitch, float v_roll);
 
 private:
