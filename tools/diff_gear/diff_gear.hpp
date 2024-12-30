f5333496502a2a75a1badf6cf8472fa2a3a4ee7c
chmwt <1102453984@qq.com>
2024-12-30 21:50:11 +0800

    Revert "Merge branch 'main' of https://github.com/TongjiSuperPower/sp_middleware"
    This reverts commit 4b37bcefb7325b07c7e50ff4cd51c06b8d1c6d9a, reversing
    changes made to ef65274bd4d0db11f13cec4d56c62f9735840efe.

--- tools/diff_gear/diff_gear.hpp
+++ tools/diff_gear/diff_gear.hpp
@@ -13,10 +13,6 @@ public:
 
   float pitch;    // 只读! calc_end_angle()计算结果, 单位: rad
   float roll;     // 只读! calc_end_angle()计算结果,  单位: rad
-  float v_pitch;  // 只读! calc_end_speed()计算结果, 单位: rad/s
-  float v_roll;   // 只读! calc_end_speed()计算结果, 单位: rad/s
-  float t_pitch;  // 只读! calc_end_torque()计算结果, 单位: N·m
-  float t_roll;   // 只读! calc_end_torque()计算结果, 单位: N·m
   float v_left;   // 只读! calc_gear_speed()计算结果, left转速, 单位: rad/s
   float v_right;  // 只读! calc_gear_speed()计算结果, right转速, 单位: rad/s
 
@@ -29,16 +25,6 @@ public:
   // angle_l: 左侧齿轮转角, 单位: rad
   // angle_r: 右侧齿轮转角, 单位: rad
   void calc_end_angle(float angle_l, float angle_r);
-
-  // 各齿轮转速 -> 末端转速
-  // speed_l: 左侧齿轮转速, 单位: rad/s
-  // speed_r: 右侧齿轮转速, 单位: rad/s
-  void calc_end_speed(float speed_l, float speed_r);
-
-  // 各齿轮转矩 -> 末端转矩
-  // torque_l: 左侧齿轮转矩, 单位: N·m
-  // torque_r: 右侧齿轮转矩, 单位: N·m
-  void calc_end_torque(float torque_l, float torque_r);
 
   // 末端转速 -> 各齿轮转速
   // v_pitch: 大拇指朝左，右手螺旋方向转速, 单位: rad/s
--- tools/motor_composer/motor_composer.cpp
+++ tools/motor_composer/motor_composer.cpp
@@ -18,16 +18,6 @@ void MotorComposer::calc_angle(float angle_l, float angle_r)
   this->angle = (sign_l_ * (angle_l - angle_l0_) + sign_r_ * (angle_r - angle_r0_)) / 2;
 }
 
-void MotorComposer::calc_speed(float speed_l, float speed_r)
-{
-  this->speed = (sign_l_ * speed_l + sign_r_ * speed_r) / 2;
-}
-
-void MotorComposer::calc_torque(float torque_l, float torque_r)
-{
-  this->torque = sign_l_ * torque_l + sign_r_ * torque_r;
-}
-
 void MotorComposer::calc_speeds(float speed)
 {
   this->speed_l = sign_l_ * speed;
