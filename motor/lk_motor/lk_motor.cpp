5005912008cb2443997563610fa0b07643a9a11f
XiaoYoung <xy_23@outlook.com>
2025-03-10 22:45:02 +0800

    update lk_motor

--- motor/lk_motor/lk_motor.cpp
+++ motor/lk_motor/lk_motor.cpp
@@ -6,9 +6,11 @@ namespace sp
 {
 constexpr float LK_MOTOR_MAX_RAW = 2048;
 constexpr float MF_RAW_TO_CURRENT = 16.5f / LK_MOTOR_MAX_RAW;
-constexpr float MF9025_CURRENT_TO_TORQUE = 0.32f;
 
-LK_Motor::LK_Motor(uint8_t motor_id) : rx_id(0x140 + motor_id), tx_id(0x280), motor_id_(motor_id) {}
+LK_Motor::LK_Motor(uint8_t motor_id, float torque_const)
+: rx_id(0x140 + motor_id), tx_id(0x280), motor_id_(motor_id), torque_const_(torque_const)
+{
+}
 
 void LK_Motor::read(const uint8_t * data)
 {
@@ -17,7 +19,7 @@ void LK_Motor::read(const uint8_t * data)
   uint16_t angle_int = (data[7] << 8) | data[6];
 
   this->temp = data[1];
-  this->torque = torque_int * MF_RAW_TO_CURRENT * MF9025_CURRENT_TO_TORQUE;
+  this->torque = torque_int * MF_RAW_TO_CURRENT * torque_const_;  // TODO MG等系列
   this->speed = speed_int / 180.0f * sp::PI;
   this->angle = sp::limit_angle(angle_int / 65535.0f * 2 * sp::PI);  // TODO 编码器不同, 系数不同
 }
@@ -30,7 +32,7 @@ void LK_Motor::write(uint8_t * data) const
 
 void LK_Motor::cmd(float value)
 {
-  float raw = value / MF9025_CURRENT_TO_TORQUE / MF_RAW_TO_CURRENT;
+  float raw = value / torque_const_ / MF_RAW_TO_CURRENT;  // TODO MG等系列
   cmd_raw_ = sp::limit_max(raw, LK_MOTOR_MAX_RAW);
 }
 
--- motor/lk_motor/lk_motor.hpp
+++ motor/lk_motor/lk_motor.hpp
@@ -5,11 +5,13 @@
 
 namespace sp
 {
+constexpr float MF9025_TORQUE_CONST = 0.32f;  // N·m/A
+
 class LK_Motor
 {
 public:
   // 仅实现了多电机模式!
-  LK_Motor(uint8_t motor_id);
+  LK_Motor(uint8_t motor_id, float torque_const = MF9025_TORQUE_CONST);
 
   const uint16_t rx_id;  // 电机反馈帧ID
   const uint16_t tx_id;  // 电机控制帧ID
@@ -26,6 +28,7 @@ public:
 
 private:
   const uint8_t motor_id_;
+  const float torque_const_;
   int16_t cmd_raw_ = 0;
 };
 
