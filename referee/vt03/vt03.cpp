1e8885a3fa18a553d907516067bda8e9722f8b58
XiaoYoung <xy_23@outlook.com>
2025-04-19 22:48:40 +0800

    update vt03

--- referee/vt03/vt03.cpp
+++ referee/vt03/vt03.cpp
@@ -9,24 +9,66 @@ VT03::VT03(UART_HandleTypeDef * huart, bool use_dma) : huart(huart), use_dma_(us
 void VT03::request()
 {
   if (use_dma_) {
-    HAL_UARTEx_ReceiveToIdle_DMA(
-      this->huart, reinterpret_cast<uint8_t *>(&remote_data_), sizeof(remote_data_));
+    HAL_UARTEx_ReceiveToIdle_DMA(this->huart, buff_.data(), sizeof(buff_));
   }
   else {
-    HAL_UART_Receive_IT(
-      this->huart, reinterpret_cast<uint8_t *>(&remote_data_), sizeof(remote_data_));
+    HAL_UARTEx_ReceiveToIdle_IT(this->huart, buff_.data(), sizeof(buff_));
   }
 }
 
 void VT03::update(uint16_t size)
 {
-  if (remote_data_.sof_1 == 0xA9) {
-    if (size != sizeof(remote_data_)) return;
-    if (remote_data_.sof_2 != 0x53) return;
-    if (!sp::check_crc16(reinterpret_cast<uint8_t *>(&remote_data_), sizeof(remote_data_))) return;
-
-    // TODO
+  if (buff_[0] == 0xA9 && buff_[1] == 0x53) {
+    if (size != sizeof(VT03RemoteData)) return;
+    if (!sp::check_crc16(buff_.data(), sizeof(VT03RemoteData))) return;
+    std::copy(buff_.begin(), buff_.end(), reinterpret_cast<uint8_t *>(&remote_data_));
+    update_remote();
   }
+
+  // TODO 0x0302
+  // else if
+}
+
+void VT03::update_remote()
+{
+  this->ch_rh = (remote_data_.ch_0 - 1024) / 660.0f;
+  this->ch_rv = (remote_data_.ch_1 - 1024) / 660.0f;
+  this->ch_lv = (remote_data_.ch_2 - 1024) / 660.0f;
+  this->ch_lh = (remote_data_.ch_3 - 1024) / 660.0f;
+  this->ch_lu = (remote_data_.wheel - 1024) / 660.0f;
+
+  this->fn_l = remote_data_.fn_1;
+  this->fn_r = remote_data_.fn_2;
+  this->pause = remote_data_.pause;
+  this->trigger = remote_data_.trigger;
+
+  this->mode = (remote_data_.mode_sw == 2)   ? VT03Mode::S
+               : (remote_data_.mode_sw == 1) ? VT03Mode::N
+                                             : VT03Mode::C;
+
+  this->mouse.vx = remote_data_.mouse_x / 32768.0f;
+  this->mouse.vy = remote_data_.mouse_y / 32768.0f;
+  this->mouse.vs = remote_data_.mouse_z / 32768.0f;
+  this->mouse.left = remote_data_.mouse_left;
+  this->mouse.right = remote_data_.mouse_right;
+  this->mouse.middle = remote_data_.mouse_middle;
+
+  this->keys.w = ((remote_data_.key & 0x0001) != 0);
+  this->keys.s = ((remote_data_.key & 0x0002) != 0);
+  this->keys.a = ((remote_data_.key & 0x0004) != 0);
+  this->keys.d = ((remote_data_.key & 0x0008) != 0);
+  this->keys.shift = ((remote_data_.key & 0x0010) != 0);
+  this->keys.ctrl = ((remote_data_.key & 0x0020) != 0);
+  this->keys.q = ((remote_data_.key & 0x0040) != 0);
+  this->keys.e = ((remote_data_.key & 0x0080) != 0);
+  this->keys.r = ((remote_data_.key & 0x0100) != 0);
+  this->keys.f = ((remote_data_.key & 0x0200) != 0);
+  this->keys.g = ((remote_data_.key & 0x0400) != 0);
+  this->keys.z = ((remote_data_.key & 0x0800) != 0);
+  this->keys.x = ((remote_data_.key & 0x1000) != 0);
+  this->keys.c = ((remote_data_.key & 0x2000) != 0);
+  this->keys.v = ((remote_data_.key & 0x4000) != 0);
+  this->keys.b = ((remote_data_.key & 0x8000) != 0);
 }
 
 }  // namespace sp
--- referee/vt03/vt03.hpp
+++ referee/vt03/vt03.hpp
@@ -1,11 +1,14 @@
 #ifndef SP__VT03_HPP
 #define SP__VT03_HPP
 
+#include <array>
+
 #include "usart.h"
 
 namespace sp
 {
-struct __attribute__((packed)) RemoteData
+
+struct __attribute__((packed)) VT03RemoteData
 {
   uint8_t sof_1;
   uint8_t sof_2;
@@ -30,20 +33,73 @@ struct __attribute__((packed)) RemoteData
   uint16_t crc16;
 };
 
+enum class VT03Mode
+{
+  C,
+  N,
+  S
+};
+
+struct VT03MouseData
+{
+  float vx;  // 取值范围: [-1, 1]
+  float vy;  // 取值范围: [-1, 1]
+  float vs;  // 取值范围: [-1, 1], 鼠标滚轮, s代表scroll
+  bool left;
+  bool right;
+  bool middle;
+};
+
+struct VT03KeysData
+{
+  bool w;
+  bool s;
+  bool a;
+  bool d;
+  bool shift;
+  bool ctrl;
+  bool q;
+  bool e;
+  bool r;
+  bool f;
+  bool g;
+  bool z;
+  bool x;
+  bool c;
+  bool v;
+  bool b;
+};
+
 class VT03
 {
 public:
   VT03(UART_HandleTypeDef * huart, bool use_dma = true);
   UART_HandleTypeDef * huart;
 
-  // TODO: remote, mouse and keys
+  float ch_rh;   // 只读! 右水平摇杆, 取值范围: [-1, 1], 右正左负
+  float ch_rv;   // 只读! 右垂直摇杆, 取值范围: [-1, 1], 上正下负
+  float ch_lh;   // 只读! 左水平摇杆, 取值范围: [-1, 1], 右正左负
+  float ch_lv;   // 只读! 左垂直摇杆, 取值范围: [-1, 1], 上正下负
+  float ch_lu;   // 只读! 左上方拨轮, 取值范围: [-1, 1], 左正右负
+  bool fn_l;     // 只读! 左自定义按键
+  bool fn_r;     // 只读! 右自定义按键
+  bool pause;    // 只读! 暂停按键
+  bool trigger;  // 只读! 扳机按键
+
+  VT03Mode mode;        // 只读! 档位切换开关
+  VT03MouseData mouse;  // 只读! 鼠标数据
+  VT03KeysData keys;    // 只读! 键盘数据
 
   void request();
   void update(uint16_t size);
 
 private:
   const bool use_dma_;
-  RemoteData remote_data_;
+
+  VT03RemoteData remote_data_;
+  std::array<uint8_t, sizeof(VT03RemoteData)> buff_;  // TODO 0x0302 size
+
+  void update_remote();
 };
 
 }  // namespace sp
