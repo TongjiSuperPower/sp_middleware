480c443e376acf0abeb5ad4b59f0b9f1dcc2e219
misedeshitou <3299751760@qq.com>
2025-05-05 20:49:28 +0800

    update adc

--- io/adc/adc.cpp
+++ io/adc/adc.cpp
@@ -3,12 +3,11 @@
 namespace sp
 {
 Adc::Adc(
-  ADC_HandleTypeDef * adc, uint32_t channel_sqr, float verf, float voltage_divider,
+  ADC_HandleTypeDef * adc, uint32_t channels, float vref, float voltage_divider,
   float voltage_offset)
-//构造函数，初始化ADC口，ADC转换位数的平方，如12位则2^12(4096)，ADC参考电压，电压分压比，电压偏移值
 : adc_(adc),
-  channel_sqr_(channel_sqr),
-  verf_(verf),
+  channel_pow_(1 << channels),
+  vref_(vref),
   voltage_divider_(voltage_divider),
   voltage_offset_(voltage_offset)
 {
@@ -20,7 +19,7 @@ void Adc::init()
   adc_->Init.ClockPrescaler = ADC_CLOCK_SYNC_PCLK_DIV4;
   HAL_ADC_Init(adc_);
   //计算ADC转换值到电压的系数
-  this->adc12_to_v_coef_ = this->verf_ * this->voltage_divider_ / this->channel_sqr_;
+  this->adc12_to_v_coef_ = this->vref_ * this->voltage_divider_ / this->channel_pow_;
 }
 
 void Adc::update()
--- io/adc/adc.hpp
+++ io/adc/adc.hpp
@@ -8,8 +8,13 @@ namespace sp
 class Adc
 {
 public:
+  // adc: adc通道数
+  // channels: adc转换位数，默认为12位
+  // vref: adc电路图参考电压 C板为3.3V
+  // voltage_divider: 分压比，C板为222.0f / 22.0f
+  // voltage_offset: 测量值与实际值的偏差
   Adc(
-    ADC_HandleTypeDef * adc, uint32_t channel_sqr, float verf, float voltage_divider,
+    ADC_HandleTypeDef * adc, uint32_t channels, float vref, float voltage_divider,
     float voltage_offset);
   void init();
   //更新得到的电压值
@@ -20,10 +25,10 @@ public:
 private:
   //adc口
   ADC_HandleTypeDef * adc_;
-  //ADC转换位数的平方，如12位则2^12(4096)
-  uint32_t channel_sqr_;
+  //2的ADC转换位数幂，如12位则2^12(4096)
+  uint32_t channel_pow_;
   //ADC参考电压
-  float verf_;
+  float vref_;
   //电压分压比
   float voltage_divider_;
   //ADC转换值到电压的系数
