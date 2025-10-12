01a076dafa397fa1db8bb950cba981dd637d6093
枝袅轻风 <2061793723@qq.com>
2025-10-12 11:36:15 +0800

    feat(lk_motor): refactor LK_Motor class - support MG series single motor mode - Replace multi-motor mode

--- motor/lk_motor/lk_motor.cpp
+++ motor/lk_motor/lk_motor.cpp
@@ -4,61 +4,167 @@
 
 namespace sp
 {
-constexpr float LK_MOTOR_MAX_RAW_TX = 2000;
-constexpr float LK_MOTOR_MAX_RAW_RX = 2048;
-constexpr float MG_RAW_TO_CURRENT_TX = 32.0f / LK_MOTOR_MAX_RAW_TX;
-constexpr float MG_RAW_TO_CURRENT_RX = 33.0f / LK_MOTOR_MAX_RAW_RX;
+
 
 LK_Motor::LK_Motor(uint8_t motor_id, float ratio, float torque_const)
 : rx_id(0x140 + motor_id), 
-  tx_id(0x280), 
-  motor_id_(motor_id), 
+  tx_id(0x140 + motor_id), 
+  motor_id_(motor_id),
   ratio_(ratio),
-  torque_const_(torque_const),
-  has_read_(false)
+  torque_const_(torque_const)
 {
 }
 
-void LK_Motor::read(const uint8_t * data)
+void LK_Motor::write_state1(uint8_t * data) const
 {
-  int16_t torque_int = (data[3] << 8) | data[2];
-  int16_t speed_int = (data[5] << 8) | data[4];
-  uint16_t angle_int = (data[7] << 8) | data[6];
+  data[0] = 0x9A;
+  data[1] = 0x00;
+  data[2] = 0x00;
+  data[3] = 0x00;
+  data[4] = 0x00;
+  data[5] = 0x00;
+  data[6] = 0x00;
+  data[7] = 0x00;
+}
+
+void LK_Motor::read_state1(const uint8_t * data)
+{
+  if (data[0] != 0x9A && data[0] != 0x9B) return;
+
+  this->temp = data[1];
+  this->motorState = data[6];
+  this->errorState = data[7];
+}
+
+void LK_Motor::write_clear_error(uint8_t * data) const
+{
+  data[0] = 0x9B;
+  data[1] = 0x00;
+  data[2] = 0x00;
+  data[3] = 0x00;
+  data[4] = 0x00;
+  data[5] = 0x00;
+  data[6] = 0x00;
+  data[7] = 0x00;
+}
+
+void LK_Motor::write_state2(uint8_t * data) const
+{
+  data[0] = 0x9C;
+  data[1] = 0x00;
+  data[2] = 0x00;
+  data[3] = 0x00;
+  data[4] = 0x00;
+  data[5] = 0x00;
+  data[6] = 0x00;
+  data[7] = 0x00;
+}
+
+void LK_Motor::read_state2(const uint8_t * data)
+{
+  if (data[0] != 0xA7 && data[0] != 0x9C && data[0] != 0xA1) return;
+
+  int16_t iq = (data[3] << 8) | data[2];
+  int16_t speed = (data[5] << 8) | data[4];
+  uint16_t encoder = (data[7] << 8) | data[6];
 
   // 首次读取时, 初始化last_ecd_
   if (!has_read_) {
     has_read_ = true;
-    last_ecd_ = angle_int;
+    last_ecd_ = encoder;
   }
 
   // 多圈编码
-  if (angle_int - last_ecd_ > 65535/2)
+  if (encoder - last_ecd_ > 65535/2)
     step_--;
-  else if (angle_int - last_ecd_ < -65535/2)
+  else if (encoder - last_ecd_ < -65535/2)
     step_++;
-  last_ecd_ = angle_int;
+  last_ecd_ = encoder;
 
-  float angle_rad = float(angle_int - 65535/2) / 65535 * 2 * PI / ratio_;
-
+  float angle_rad = float(encoder - 65535/2) / 65535 * 2 * PI / ratio_;
 
   this->temp = data[1];
-  this->torque = torque_int * MG_RAW_TO_CURRENT_RX * torque_const_;
-  this->speed = -speed_int / 180.0f * sp::PI / ratio_;
-  // this->angle = float(angle_int / 65535.0f * 2 * sp::PI / ratio_);
-  this->angle = -sp::limit_angle(angle_rad + step_ * 2 * PI / ratio_);
-
+  this->torque = iq * ( 66.0f / 4096.0f ) * torque_const_ * ratio_;
+  this->speed = speed / 180.0f * sp::PI / ratio_;
+  this->angle = sp::limit_angle(angle_rad + step_ * 2 * PI / ratio_);
 }
 
-void LK_Motor::write(uint8_t * data) const
+void LK_Motor::write_turn_off(uint8_t * data) const
 {
-  data[(motor_id_ - 1) % 4 * 2 + 0] = cmd_raw_;
-  data[(motor_id_ - 1) % 4 * 2 + 1] = cmd_raw_ >> 8;
+  data[0] = 0x80;
+  data[1] = 0x00;
+  data[2] = 0x00;
+  data[3] = 0x00;
+  data[4] = 0x00;
+  data[5] = 0x00;
+  data[6] = 0x00;
+  data[7] = 0x00;
 }
 
-void LK_Motor::cmd(float value)
+void LK_Motor::write_turn_on(uint8_t * data) const
 {
-  float raw = value / torque_const_ / MG_RAW_TO_CURRENT_TX;
-  cmd_raw_ = sp::limit_max(raw, LK_MOTOR_MAX_RAW_TX);
+  data[0] = 0x88;
+  data[1] = 0x00;
+  data[2] = 0x00;
+  data[3] = 0x00;
+  data[4] = 0x00;
+  data[5] = 0x00;
+  data[6] = 0x00;
+  data[7] = 0x00;
 }
+
+void LK_Motor::write_stop(uint8_t * data) const
+{
+  data[0] = 0x81;
+  data[1] = 0x00;
+  data[2] = 0x00;
+  data[3] = 0x00;
+  data[4] = 0x00;
+  data[5] = 0x00;
+  data[6] = 0x00;
+  data[7] = 0x00;
+}
+
+void LK_Motor::write_position(uint8_t * data) const
+{
+  data[0] = 0xA5;
+  data[1] = spin_direction_;
+  data[4] = cmd_position_;
+  data[5] = cmd_position_ >> 8;
+  data[6] = cmd_position_ >> 16;
+  data[7] = cmd_position_ >> 24;
+}
+
+void LK_Motor::write_angle_increment(uint8_t * data) const
+{
+  data[0] = 0xA7;
+  data[4] = cmd_angle_;
+  data[5] = cmd_angle_ >> 8;
+  data[6] = cmd_angle_ >> 16;
+  data[7] = cmd_angle_ >> 24;
+}
+
+void LK_Motor::write_torque(uint8_t * data) const
+{
+  data[0] = 0xA1;
+  data[4] = cmd_raw_;
+  data[5] = cmd_raw_ >> 8;
+}
+
+void LK_Motor::cmd_position(int32_t position, uint8_t direction) 
+{
+  cmd_position_ = position * 100;
+  spin_direction_ = direction;
+}
+
+void LK_Motor::cmd_angle(int32_t angle) { cmd_angle_ = angle * 1000; }
+
+
+void LK_Motor::cmd_torque(float value)
+{
+  float raw = value / torque_const_ / (33.0f / 2048.f) / ratio_;  // TODO MG等系列
+  cmd_raw_ = sp::limit_max(raw, 2048.0f);
+}
+
 
 }  // namespace sp
--- motor/lk_motor/lk_motor.hpp
+++ motor/lk_motor/lk_motor.hpp
@@ -5,37 +5,73 @@
 
 namespace sp
 {
-constexpr float MG4005i10_TORQUE_CONST = 0.06f;  // N·m/A
 constexpr float MG4005i10_P10 = 10.0f;           // 减速比
+constexpr float MG4005i10_TORQUE_CONST = 0.06f;  // N·m/A
 
 class LK_Motor
 {
 public:
-  // 仅实现了多电机模式!
+  // 仅实现了MG系列单电机模式!
+  // motor_type: 电机型号， 取值见LK_Motors`
+  // ratio: 减速比
   LK_Motor(uint8_t motor_id, float ratio = MG4005i10_P10, float torque_const = MG4005i10_TORQUE_CONST);
 
   const uint16_t rx_id;  // 电机反馈帧ID
   const uint16_t tx_id;  // 电机控制帧ID
+
+  uint8_t motorState = 0x10;
+  uint8_t errorState = 0;
 
   float angle = 0;   // 只读! 单位: rad
   float speed = 0;   // 只读! 单位: rad/s
   float torque = 0;  // 只读! 单位: N·m
   int8_t temp = 0;   // 只读! 单位: 摄氏度
 
-  void read(const uint8_t * data);
-  void write(uint8_t * data) const;
+  // 该命令读取当前电机的温度、电压和错误状态标志
+  void write_state1(uint8_t * data) const;
+  void read_state1(const uint8_t * data);
 
-  void cmd(float value);
+  // 电机状态没有恢复正常时，错误标志无法清除
+  void write_clear_error(uint8_t * data) const;
+
+  // 该命令读取当前电机的温度、电机转矩电流、转速、编码器位置
+  void write_state2(uint8_t * data) const;
+  void read_state2(const uint8_t * data);
+
+  // 将电机从开启状态切换到关闭状态，清除电机转动圈数及之前接收的控制指令，此时电机仍然可以回复控制命令，但不会执行动作
+  void write_turn_off(uint8_t * data) const;
+  // 将电机从关闭状态切换到开启状态
+  void write_turn_on(uint8_t * data) const;
+  // 停止电机，但不清除电机运行状态，再次发送控制指令即可控制电机动作
+  void write_stop(uint8_t * data) const;
+
+  // 主机发送该命令以控制电机的位置（单圈角度）
+  void write_position(uint8_t * data) const;
+  // 主机发送该命令以控制电机的位置增量
+  void write_angle_increment(uint8_t * data) const;
+  // 主机发送该命令以控制电机的转矩电流输出
+  void write_torque(uint8_t * data) const;
+
+  // 控制值 direction 设置电机转动的方向，为 uint8_t 类型，0x00 代表顺时针，0x01 代表逆时针
+  void cmd_position(int32_t position, uint8_t direction);
+  void cmd_angle(int32_t angle);
+  void cmd_torque(float value);
 
 private:
   const uint8_t motor_id_;
   const float ratio_;
   const float torque_const_;
-  bool has_read_;
-  int16_t cmd_raw_ = 0;
 
+  int32_t cmd_angle_;
+  int16_t cmd_raw_ = 0;
+  
+  bool has_read_;
   int32_t step_;
   uint16_t last_ecd_;
+
+  uint32_t cmd_position_;
+  uint8_t spin_direction_;
+
 };
 
 }  // namespace sp
