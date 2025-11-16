673424841b89cc155eb35f21124028e1ec46b66b
konichan-7 <2250624@tongji.edu.cn>
2025-11-16 20:59:17 +0800

    fix(gimbal) : some errors in update function

--- tools/gimbal/gimbal.cpp
+++ tools/gimbal/gimbal.cpp
@@ -73,13 +73,12 @@ void Gimbal::update(
                           sinf(pitch_realative_angle) * sinf(yaw_realative_angle) +
                         sinf(yaw_realative_angle) * cosf(gimbal_imu.pitch) * cosf(gimbal_imu.yaw) *
                           cosf(pitch_realative_angle);
-  R_base2world_[0][1] = -R_base2world_[0][1];
   R_base2world_[0][2] = (sinf(gimbal_imu.pitch) * cosf(gimbal_imu.roll) * cosf(gimbal_imu.yaw) +
                          sinf(gimbal_imu.roll) * sinf(gimbal_imu.yaw)) *
                           cosf(pitch_realative_angle) -
                         sinf(pitch_realative_angle) * cosf(gimbal_imu.pitch) * cosf(gimbal_imu.yaw);
-  R_base2world_[1][0] = (sinf(gimbal_imu.pitch) * sinf(gimbal_imu.roll) * sinf(gimbal_imu.yaw) +
-                         cosf(gimbal_imu.roll) * cosf(gimbal_imu.yaw)) *
+  R_base2world_[1][0] = -(sinf(gimbal_imu.pitch) * sinf(gimbal_imu.roll) * sinf(gimbal_imu.yaw) +
+                          cosf(gimbal_imu.roll) * cosf(gimbal_imu.yaw)) *
                           sinf(yaw_realative_angle) +
                         (sinf(gimbal_imu.pitch) * sinf(gimbal_imu.yaw) * cosf(gimbal_imu.roll) -
                          sinf(gimbal_imu.roll) * cosf(gimbal_imu.yaw)) *
@@ -98,7 +97,6 @@ void Gimbal::update(
                          sinf(gimbal_imu.roll) * cosf(gimbal_imu.yaw)) *
                           cosf(pitch_realative_angle) -
                         sinf(gimbal_imu.yaw) * sinf(pitch_realative_angle) * cosf(gimbal_imu.pitch);
-  R_base2world_[1][2] = -R_base2world_[1][2];
   R_base2world_[2][0] =
     -sinf(gimbal_imu.pitch) * cosf(pitch_realative_angle) * cosf(yaw_realative_angle) -
     sinf(gimbal_imu.roll) * sinf(yaw_realative_angle) * cosf(gimbal_imu.pitch) +
@@ -109,7 +107,6 @@ void Gimbal::update(
     sinf(gimbal_imu.roll) * cosf(gimbal_imu.pitch) * cosf(yaw_realative_angle) +
     sinf(pitch_realative_angle) * sinf(yaw_realative_angle) * cosf(gimbal_imu.pitch) *
       cosf(gimbal_imu.roll);
-  R_base2world_[2][1] = -R_base2world_[2][1];
   R_base2world_[2][2] =
     sinf(gimbal_imu.pitch) * sinf(pitch_realative_angle) +
     cosf(gimbal_imu.pitch) * cosf(gimbal_imu.roll) * cosf(pitch_realative_angle);
