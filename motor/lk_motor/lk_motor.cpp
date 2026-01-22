21905f98185f14cacae9b27e034ad8acced32a60
Ni <1835651172@qq.com>
2026-01-22 20:42:07 +0800

    feat: add multi_circle of lk

--- motor/lk_motor/lk_motor.cpp
+++ motor/lk_motor/lk_motor.cpp
@@ -7,8 +7,14 @@ namespace sp
 constexpr float LK_MOTOR_MAX_RAW = 2048;
 constexpr float MF_RAW_TO_CURRENT = 16.5f / LK_MOTOR_MAX_RAW;
 
-LK_Motor::LK_Motor(uint8_t motor_id, float torque_const)
-: rx_id(0x140 + motor_id), tx_id(0x280), motor_id_(motor_id), torque_const_(torque_const)
+LK_Motor::LK_Motor(uint8_t motor_id, float torque_const, bool multi_circle)
+: rx_id(0x140 + motor_id),
+  tx_id(0x280),
+  motor_id_(motor_id),
+  torque_const_(torque_const),
+  multi_circle_(multi_circle),
+  has_read_(false),
+  circle_(0)
 {
 }
 
@@ -21,7 +27,18 @@ void LK_Motor::read(const uint8_t * data)
   this->temp = data[1];
   this->torque = torque_int * MF_RAW_TO_CURRENT * torque_const_;  // TODO MG等系列
   this->speed = speed_int / 180.0f * sp::SP_PI;
-  this->angle = sp::limit_angle(angle_int / 65535.0f * 2 * sp::SP_PI);  // TODO 编码器不同, 系数不同
+  this->angle_raw_ =
+    sp::limit_angle(angle_int / 65535.0f * 2 * sp::SP_PI);  // TODO 编码器不同, 系数不同
+  if (!has_read_) {
+    has_read_ = true;
+    last_angle_raw_ = angle_raw_;
+  }
+  if (angle_raw_ - last_angle_raw_ > sp::SP_PI)
+    circle_--;
+  else if (angle_raw_ - last_angle_raw_ < -sp::SP_PI)
+    circle_++;
+  last_angle_raw_ = angle_raw_;
+  this->angle = multi_circle_ ? angle_raw_ + circle_ * 2 * SP_PI : angle_raw_;
 }
 
 void LK_Motor::write(uint8_t * data) const
--- motor/lk_motor/lk_motor.hpp
+++ motor/lk_motor/lk_motor.hpp
@@ -12,7 +12,9 @@ class LK_Motor
 {
 public:
   // 仅实现了多电机模式!
-  LK_Motor(uint8_t motor_id, float torque_const = MF4005_TORQUE_CONST);  // 默认型号: MF4005
+  LK_Motor(
+    uint8_t motor_id, float torque_const = MF4005_TORQUE_CONST,
+    bool multi_circle = false);  // 默认型号: MF4005
 
   const uint16_t rx_id;  // 电机反馈帧ID
   const uint16_t tx_id;  // 电机控制帧ID
@@ -31,6 +33,12 @@ private:
   const uint8_t motor_id_;
   const float torque_const_;
   int16_t cmd_raw_ = 0;
+
+  const bool multi_circle_;
+  bool has_read_;
+  int32_t circle_;
+  float angle_raw_ = 0;
+  float last_angle_raw_;
 };
 
 }  // namespace sp
